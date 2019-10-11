81c36120b98ef79dea71f903bdaf0eb33e04bf20
Roy Wakumelo Jr <roywakumelo@roys-mbp.attlocal.net>
2019-10-11 11:31:53 -0400

    Project Set Up Complete

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+
+const store = createStore ( applyMiddleware );
+ReactDOM.render(
+    <Provider store={store}>
+        <App /> 
+    </Provider>,
+    document.getElementById("root")
+);
